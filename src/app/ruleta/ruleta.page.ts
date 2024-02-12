a8005cf40dae47c81b96594c57be79b4350cba00
Romanrr-06 <150660054+Romanrr-06@users.noreply.github.com>
2024-02-12 10:39:03 +0100

    12

--- src/app/ruleta/ruleta.page.ts
+++ src/app/ruleta/ruleta.page.ts
@@ -1,7 +1,7 @@
 // ruleta.page.ts
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+// import { environment } from 'src/environments/environment';
 interface CryptoOption {
   id: number;
   name: string;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { Host } from "@angular/core"
+
 export const environment = {
-  production: false
+  production: true,
+  bakend_Host:'http://localhost:3000'
 };
 
 /*
